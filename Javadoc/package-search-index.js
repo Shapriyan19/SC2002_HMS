a147ca8d2b4dcec7719a05b721faa466c31779bf
Shapriyan19 <priyanmartian@gmail.com>
2024-11-17 16:58:44 +0800

    javadoc added

---
+++ Javadoc/package-search-index.js
@@ -0,0 +1,1 @@
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"appointment"},{"l":"billing"},{"l":"hmsApp"},{"l":"inventory"},{"l":"medical"},{"l":"user"}];updateSearchResults();
